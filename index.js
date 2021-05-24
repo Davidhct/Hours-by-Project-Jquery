9fdbea903268539bdf63b6cb0ab396a5d5b49d9c
Davidhct <davidhct1@gmail.com>
2021-05-24 18:29:44 +0300

    update css, html and js files

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 document.getElementById("open").addEventListener("click", openMenu);
 document.getElementById("close").addEventListener("click", closeMenu);
 const container = document.querySelector(".container");
+const circle = document.querySelector(".circle");
 
 function openMenu() {
   container.classList.add("show-nav");
+  circle.classList.add("color");
 }
 function closeMenu() {
   container.classList.remove("show-nav");
+  circle.classList.remove("color");
 }
